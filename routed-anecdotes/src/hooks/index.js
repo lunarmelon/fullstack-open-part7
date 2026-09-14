5b09b580075c81e73dda1073383d584663914edc
lunarmelon <lunarmelon@tuta.io>
2026-09-14 17:13:16 -0600

    exercise 7.4 - useAnecdotes step 1

--- routed-anecdotes/src/hooks/index.js
+++ routed-anecdotes/src/hooks/index.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import anecdoteService from "../services/anecdotes";
 
 export const useField = (type) => {
 	const [value, setValue] = useState("");
@@ -17,4 +18,14 @@ export const useField = (type) => {
 		onChange,
 		onReset,
 	};
+};
+
+export const useAnecdotes = () => {
+	const [anecdotes, setAnecdotes] = useState([]);
+
+	useEffect(() => {
+		anecdoteService.getAll().then((data) => setAnecdotes(data));
+	}, []);
+
+	return anecdotes;
 };
